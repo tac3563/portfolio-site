d28c4723c6315718a61f122df01c3f2e23da7337
tac3563 <tcox_bu@outlook.com>
2025-07-27 19:31:53 +0100

    [Update] initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import PageHeader from './components/PageHeader.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        {/*<NavBar/>*/}
+        <PageHeader/>
+        {/*<ProjectGrid/>*/}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,8 @@
+export default function PageHeader() {
+    return (
+        <>
+            <h1>Tom Cox // Frontend Developer</h1>
+            <p>Driven by the opportunity to bring your design visions to life, I love crafting practical, purposeful, and seamless design solutions that build trust and bridge the gap between ideas and reality.</p>
+        </>
+    )
+}
